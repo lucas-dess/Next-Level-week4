7f1031914a8881a9d296d2af19afba62ad93cbda
Lucas de Souza Silva <74208740+lucas-dess@users.noreply.github.com>
2021-02-25 15:30:16 -0300

    dia 3

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { Profile } from "../components/Profile";
 import Head from 'next/head';
 
 import styles from '../styles/pages/Home.module.css';
+import { ChallangeBox } from "../components/ChallengeBox";
 
 export default function Home() {
   return (
@@ -19,7 +20,7 @@ export default function Home() {
           <Countdown />
         </div>
         <div>
-
+          <ChallangeBox />
         </div>
       </section>
 
